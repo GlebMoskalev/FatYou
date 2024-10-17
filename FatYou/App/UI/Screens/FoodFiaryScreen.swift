684f13415c516685fd939b39154e367097008833
GlebMoskalev <gleb12122003@gmail.com>
2024-10-17 15:03:15 +0500

    add tabBar

---
+++ FatYou/App/UI/Screens/FoodFiaryScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FoodFiaryScreen.swift
+//  FatYou
+//
+//  Created by Глеб Москалев on 17.10.2024.
+//
+
+import SwiftUI
+
+struct FoodFiaryScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FoodFiaryScreen()
+}
---
+++ FatYou/Resources/Icons/CustomIcons.swift
@@ -0,0 +1,8 @@
+//
+//  CustomIcons.swift
+//  FatYou
+//
+//  Created by Глеб Москалев on 17.10.2024.
+//
+
+import Foundation
