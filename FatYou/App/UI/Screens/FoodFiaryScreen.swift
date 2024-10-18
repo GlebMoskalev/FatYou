21d04c06056e49fe95b758cef1f9a8ff0c5f340e
GlebMoskalev <gleb12122003@gmail.com>
2024-10-18 17:28:57 +0500

    add PacmanProgress

--- FatYou/App/UI/Screens/FoodFiaryScreen.swift
+++ FatYou/App/UI/Screens/FoodFiaryScreen.swift
@@ -60,21 +60,49 @@ struct FoodFiaryScreen: View {
 }
 
 private struct CaloryInfo: View {
+    @State private var isCollapsed = false
+    
     var body: some View {
-        HStack(spacing: 0){
-            Button {
+        VStack(spacing: 0) {
+            HStack(spacing: 0){
+                Button {
+                    withAnimation {
+                        isCollapsed.toggle()
+                    }
+                } label: {
+                    HStack(alignment: .bottom, spacing: 11){
+                        Text("235 cal left")
+                            .customFont(.h2)
+                            .lineLimit(1)
+                        Image(.upSmall)
+                            .rotationEffect(isCollapsed ? .degrees(180) : .zero)
+                    }
+                    .foregroundColor(.mainText)
+                }
+                .frame(minWidth: 100)
                 
-            } label: {
-                Text("123")
-            }
-            Spacer()
-            Button {
+                Spacer()
                 
-            } label: {
-                Text("123")
+                Button {
+                    
+                } label: {
+                    Text("Add Food")
+                        .lineLimit(1)
+                        .customFont(.smallButtonTxt)
+                        .foregroundStyle(.white)
+                        .padding(EdgeInsets(top: 8, leading: 14, bottom: 8, trailing: 14))
+                        .background(.accent)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .layoutPriority(1)
             }
+            PacmanProgress(percent: 1.0)
+                .padding(.top, 16)
+                .frame(maxHeight: isCollapsed ? 0 : .infinity)
+                .opacity(isCollapsed ? 0 : 1)
+                .clipped()
         }
-        .padding(EdgeInsets(top: 20, leading: 17, bottom: 23, trailing: 17))
+        .padding(EdgeInsets(top: 20, leading: 20, bottom: 23, trailing: 17))
         .background(.white)
         .clipShape(
             UnevenRoundedRectangle(
@@ -82,6 +110,33 @@ private struct CaloryInfo: View {
                     topLeading: 20,
                     topTrailing: 20
                 )))
+    }
+}
+
+private struct PacmanProgress: View {
+    let percent: Double
+    
+    var body: some View {
+        let headSize = 60.0
+        let dotSize = 14.0
+        let dotSpace = 5.0
+        let rightPadding = 20.0
+        GeometryReader { proxy in
+            let minSize = headSize / 2
+            let actualSize = (proxy.size.width - minSize) * percent
+            let dotCount = Int(actualSize / (dotSize + dotSpace))
+            
+            ZStack (alignment: .leading){
+                HStack(spacing: dotSpace){
+                    ForEach(0..<dotCount, id: \.self) { _ in
+                        Circle()
+                            .fill(.lightBlue)
+                            .frame(width: dotSize, height: dotSize)
+                    }
+                }.padding(.leading, headSize / 2)
+                Image(.packmanHead)
+            }.frame(maxWidth: .infinity, alignment: .trailing)
+        }.frame(height: headSize)
     }
 }
 
